d217a647df54d85082f9e73f380051b2bb6db81b
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2023-01-11 16:37:13 +0100

    _onehot_encoding is able to deal with pre-fitted classifiers that use labels starting at 1

--- qunfold/transformers.py
+++ qunfold/transformers.py
@@ -2,10 +2,14 @@ import numpy as np
 from abc import ABC, abstractmethod
 
 # helper function for crisp transformations
-def _onehot_encoding(y, n_classes=None):
-  if n_classes is None:
-    n_classes = len(np.unique(y))
-  return np.eye(n_classes)[y] # https://stackoverflow.com/a/42874726/20580159
+def _onehot_encoding(y, classes=None):
+  if classes is None:
+    classes = np.unique(y)
+  if classes.min() == 1:
+    y -= 1
+  elif classes.min() != 0:
+    raise ValueError("classes.min() ∉ [0, 1]")
+  return np.eye(len(classes))[y] # https://stackoverflow.com/a/42874726/20580159
 
 class AbstractTransformer(ABC):
   """Abstract base class for transformers."""
@@ -34,10 +38,10 @@ class ClassTransformer(AbstractTransformer):
     fX = fX[is_finite,:]
     y = y[is_finite]
     if not self.is_probabilistic:
-      fX = _onehot_encoding(np.argmax(fX, axis=1), len(self.classifier.classes_))
+      fX = _onehot_encoding(np.argmax(fX, axis=1), self.classifier.classes_)
     return fX, y
   def transform(self, X):
     if self.is_probabilistic:
       return self.classifier.predict_proba(X)
     else:
-      return _onehot_encoding(self.classifier.predict(X), len(self.classifier.classes_))
+      return _onehot_encoding(self.classifier.predict(X), self.classifier.classes_)
