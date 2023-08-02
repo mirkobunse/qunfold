a2c1dc652a801a7d74bdc261a259af82071718b0
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2023-08-02 12:13:55 +0200

    Condition number experiment

--- qunfold/experiments/lequa.py
+++ qunfold/experiments/lequa.py
@@ -7,100 +7,114 @@ import quapy as qp
 from datetime import datetime
 from functools import partial
 from multiprocessing import Pool
-from qunfold import ACC, PACC, HDy, EDy, RUN, ClassTransformer
+from qunfold import ClassTransformer, GenericMethod, LeastSquaresLoss, BlobelLoss
 from qunfold.quapy import QuaPyWrapper
 from qunfold.sklearn import CVClassifier
-from sklearn.ensemble import BaggingClassifier
 from sklearn.linear_model import LogisticRegression
-from tqdm.auto import tqdm
 
-# for debugging; this variant raises exceptions instead of hiding them
-import signal
-from copy import deepcopy
-from time import time
-class MyGridSearchQ(qp.model_selection.GridSearchQ):
-    def _delayed_eval(self, args):
-        params, training = args
-        protocol = self.protocol
-        error = self.error
-        if self.timeout > 0:
-            def handler(signum, frame):
-                raise TimeoutError()
-            signal.signal(signal.SIGALRM, handler)
-        tinit = time()
-        if self.timeout > 0:
-            signal.alarm(self.timeout)
-        try:
-            model = deepcopy(self.model)
-            model.set_params(**params)
-            model.fit(training)
-            score = qp.evaluation.evaluate(model, protocol=protocol, error_metric=error)
-            ttime = time()-tinit
-            self._sout(f'hyperparams={params}\t got {error.__name__} score {score:.5f} [took {ttime:.4f}s]')
-            if self.timeout > 0:
-                signal.alarm(0)
-        except TimeoutError:
-            self._sout(f'timeout ({self.timeout}s) reached for config {params}')
-            score = None
-        return params, score, model
+def condition_number(M, norm="blobel"):
+    """Compute the condition number of a matrix"""
+    if norm != "blobel": # norm in [None, 1, -1, 2, -2, inf, -inf, "fro"]
+        return np.linalg.cond(M, norm)
+    s = np.linalg.svd(M, compute_uv=False) # norm="blobel" equals norm="2" and norm="None"
+    s = s[s != 0]
+    return s[0] / s[-1]
 
-def trial(trial_config, trn_data, val_gen, tst_gen, seed, n_trials):
+class PseudoInverse(GenericMethod):
+    def __init__(self, transformer, normalize=True, **kwargs):
+        GenericMethod.__init__(self, None, transformer, **kwargs)
+        self.normalize = normalize
+    def solve(self, q, M, N=None): # overwrite GenericMethod.solve
+        p = np.dot(np.linalg.pinv(M), q) # solve via pseudo-inverse
+        if self.normalize:
+            p = np.maximum(0, p) # clip values that are smaller than 0
+            return p / p.sum() # normalize to 1
+        return p # not a probability density
+
+def trial(transformer_config, trn_data, val_gen, seed, n_trials):
     """A single trial of lequa.main()"""
-    i_method, method_name, package, method, param_grid, error_metric = trial_config
+    i_transformer, is_probabilistic, class_weight, C, transformer = transformer_config
     np.random.seed(seed)
     print(
-        f"INI [{i_method+1:02d}/{n_trials:02d}]:",
+        f"INI [{i_transformer+1:02d}/{n_trials:02d}]:",
         datetime.now().strftime('%H:%M:%S'),
-        f"{package} {method_name} / {error_metric} starting"
+        f"p={is_probabilistic}, w={class_weight}, C={C} starting"
     )
 
-    # configure and train the method; select the best hyper-parameters
-    # quapy_method = qp.model_selection.GridSearchQ(
-    quapy_method = MyGridSearchQ(
-        model = method,
-        param_grid = param_grid,
-        protocol = val_gen,
-        error = "m" + error_metric, # ae -> mae, rae -> mrae
-        refit = False,
-        # verbose = True,
-    ).fit(trn_data)
-    parameters = quapy_method.best_params_
-    val_error = quapy_method.best_score_
-    quapy_method = quapy_method.best_model_
-    print(
-        f"VAL [{i_method+1:02d}/{n_trials:02d}]:",
-        datetime.now().strftime('%H:%M:%S'),
-        f"{package} {method_name} validated {error_metric}={val_error:.4f} {parameters}"
-    )
+    results = []
+    solvers = [ # (solver_name, generic_method)
+        ("lsq", GenericMethod(LeastSquaresLoss(), transformer)),
+        ("blobel", GenericMethod(BlobelLoss(), transformer)),
+        ("pinv-normalize", PseudoInverse(transformer, normalize=True)),
+        ("pinv-nonormalize", PseudoInverse(transformer, normalize=False)),
+    ]
+    for i_solver, (solver_name, generic_method) in enumerate(solvers):
+        quapy_method = QuaPyWrapper(generic_method).fit(trn_data)
 
-    # evaluate the method on the test samples and return the result
-    errors = qp.evaluation.evaluate( # errors of all predictions
-        quapy_method,
-        protocol = tst_gen,
-        error_metric = error_metric
-    )
-    error = errors.mean()
-    error_std = errors.std()
-    print(
-        f"TST [{i_method+1:02d}/{n_trials:02d}]:",
-        datetime.now().strftime('%H:%M:%S'),
-        f"{package} {method_name} tested {error_metric}={error:.4f}+-{error_std:.4f}"
-    )
-    return {
-        "method": method_name,
-        "package": package,
-        "error_metric": error_metric,
-        "error": error,
-        "error_std": error_std,
-        "val_error": val_error,
-        "parameters": str(parameters),
-    }
+        # compute all condition numbers
+        condition_numbers = {}
+        for norm in [ 1, -1, 2, -2, np.inf, -np.inf, "fro" ]:
+            condition_numbers["cond_" + str(norm)] = condition_number(
+                quapy_method.generic_method.M,
+                norm
+            )
+
+        # evaluate the method on the validation samples
+        errors = {}
+        p_true, p_pred = qp.evaluation.prediction(quapy_method, val_gen)
+        for error_metric in [ "ae", "rae", "se" ]:
+            error_values = qp.error.from_name(error_metric)(p_true, p_pred)
+            errors[error_metric] = error_values.mean()
+            errors[error_metric + "_std"] = error_values.std()
+
+        solver_dict = {
+            "is_probabilistic": is_probabilistic,
+            "class_weight": class_weight,
+            "C": C,
+            "solver": solver_name,
+        }
+        results.append(solver_dict | condition_numbers | errors)
+        print(
+            f"VAL [{i_transformer+1:02d}/{n_trials:02d}]:",
+            datetime.now().strftime('%H:%M:%S'),
+            f"p={is_probabilistic}, w={class_weight}, C={C} validated with {solver_name} ({i_solver+1}/{len(solvers)})"
+        )
+    return results
+
+def fit_classifier(classifier_config, trn_data, seed, is_test_run, n_classifiers):
+    i_classifier, class_weight, C = classifier_config
+    np.random.seed(seed)
+    classifier = CVClassifier(
+        LogisticRegression(
+            class_weight = class_weight,
+            C = C,
+            max_iter = 100 if not is_test_run else 3,
+        ),
+        n_estimators = 5 if not is_test_run else 3,
+        random_state = seed,
+    ).fit(*trn_data.Xy)
+    transformers = []
+    for is_probabilistic in [ True, False ]:
+        transformers.append((
+            str(is_probabilistic),
+            str(class_weight),
+            C,
+            ClassTransformer(
+                classifier,
+                is_probabilistic = is_probabilistic,
+                fit_classifier = False
+            )
+        ))
+    return transformers
 
 def main(
         output_path,
         seed = 867,
+        n_jobs = -1,
         is_test_run = False,
     ):
+    if n_jobs < 0:
+        n_jobs = None
     print(f"Starting a lequa experiment to produce {output_path} with seed {seed}")
     if is_test_run:
         print("WARNING: this is a test run; results are not meaningful")
@@ -111,118 +125,54 @@ def main(
     qp.environ["SAMPLE_SIZE"] = 1000
     qp.environ["N_JOBS"] = 1
 
-    # configure the quantification methods
-    clf = CVClassifier(
-        LogisticRegression(),
-        n_estimators = 5,
-        random_state = seed,
-    )
-    clf_grid = {
-        "transformer__classifier__estimator__C": [1e-3, 1e-2, 1e-1, 1e0, 1e1],
-    }
-    qp_clf = clf.estimator
-    qp_clf_grid = {
-        "classifier__C": clf_grid["transformer__classifier__estimator__C"],
-    }
-    methods = [ # (method_name, package, method, param_grid)
-        ("ACC", "qunfold", QuaPyWrapper(ACC(clf, seed=seed)), clf_grid),
-        ("ACC", "QuaPy", qp.method.aggregative.ACC(qp_clf, val_split=5), qp_clf_grid),
-        ("PACC", "qunfold", QuaPyWrapper(PACC(clf, seed=seed)), clf_grid),
-        ("PACC", "QuaPy", qp.method.aggregative.PACC(qp_clf, val_split=5), qp_clf_grid),
-        ("HDy", "qunfold",
-            QuaPyWrapper(HDy(clf, 2, seed=seed)),
-            {
-                "transformer__preprocessor__classifier__estimator__C":
-                    clf_grid["transformer__classifier__estimator__C"],
-                "transformer__n_bins": [2, 4, 6],
-            }
-        ),
-        ("HDy", "QuaPy",
-            qp.method.aggregative.DistributionMatching(
-                classifier = qp_clf,
-                divergence = 'HD',
-                cdf = False
-            ),
-            dict(qp_clf_grid, nbins = [2, 4, 6]) # extend the qp_clf_grid
-        ),
-        ("EDy", "qunfold",
-            QuaPyWrapper(EDy(clf, seed=seed)),
-            {
-                "transformer__preprocessor__classifier__estimator__C":
-                    clf_grid["transformer__classifier__estimator__C"],
-            }
-        ),
-        ("RUN", "qunfold", QuaPyWrapper(RUN(ClassTransformer(clf), seed=seed)), clf_grid),
-    ]
-
-    # load the data
-    trn_data, val_gen, tst_gen = qp.datasets.fetch_lequa2022(task="T1B")
-
-    # for now, only a subset of the samples is used; TODO use all samples
-    val_gen.true_prevs.df = val_gen.true_prevs.df[:100]
-    tst_gen.true_prevs.df = tst_gen.true_prevs.df[:500]
-
+    # load the data; subsample for testing
+    trn_data, val_gen, _ = qp.datasets.fetch_lequa2022(task="T1B")
+    val_gen.true_prevs.df = val_gen.true_prevs.df[:100] # TODO use all samples
     if is_test_run: # use a minimal testing configuration
-        clf.set_params(n_estimators = 3, estimator__max_iter = 3)
-        clf_grid = {
-            "transformer__classifier__estimator__C": [1e1],
-        }
-        qp_clf = clf.estimator
-        qp_clf_grid = {
-            "classifier__C": clf_grid["transformer__classifier__estimator__C"],
-        }
-        methods = [ # (method_name, package, method, param_grid)
-            ("ACC", "qunfold", QuaPyWrapper(ACC(clf, seed=seed)), clf_grid),
-            ("ACC", "QuaPy", qp.method.aggregative.ACC(qp_clf, val_split=3), qp_clf_grid),
-            ("PACC", "qunfold", QuaPyWrapper(PACC(clf, seed=seed)), clf_grid),
-            ("PACC", "QuaPy", qp.method.aggregative.PACC(qp_clf, val_split=3), qp_clf_grid),
-            ("HDy", "qunfold",
-                QuaPyWrapper(HDy(clf, 2, seed=seed)),
-                {
-                    "transformer__preprocessor__classifier__estimator__C":
-                        clf_grid["transformer__classifier__estimator__C"],
-                    "transformer__n_bins": [2, 4],
-                }
-            ),
-            ("HDy", "QuaPy",
-                qp.method.aggregative.DistributionMatching(
-                    classifier = qp_clf,
-                    divergence = 'HD',
-                    cdf = False
-                ),
-                dict(qp_clf_grid, nbins = [2, 4]) # extend the qp_clf_grid
-            ),
-            ("EDy", "qunfold",
-                QuaPyWrapper(EDy(clf, seed=seed)),
-                {
-                    "transformer__preprocessor__classifier__estimator__C":
-                        clf_grid["transformer__classifier__estimator__C"],
-                }
-            ),
-            ("RUN", "qunfold", QuaPyWrapper(RUN(ClassTransformer(clf), seed=seed)), clf_grid),
-        ]
         trn_data = trn_data.split_stratified(3000, random_state=seed)[0] # subsample
         val_gen.true_prevs.df = val_gen.true_prevs.df[:3] # use only 3 validation samples
-        tst_gen.true_prevs.df = tst_gen.true_prevs.df[:3] # use only 3 testing samples
 
-    # parallelize over all methods
-    error_metrics = ['ae', 'rae']
+
+    # fit the classifiers and create transformers
+    class_weights = [ None, "balanced" ]
+    C_values = [ 1e-3, 1e-2, 1e-1, 1e0, 1e1 ]
+    if is_test_run:
+        C_values = C_values[:1]
+    classifier_configs = [ # (i_classifier, class_weight, C)
+        (x[0], x[1][0], x[1][1])
+        for x in enumerate(itertools.product(class_weights, C_values))
+    ]
+    configured_fit_classifier = partial(
+        fit_classifier,
+        trn_data = trn_data,
+        seed = seed,
+        is_test_run = is_test_run,
+        n_classifiers = len(classifier_configs),
+    )
+    print(f"Fitting {len(classifier_configs)} classifiers")
+    transformer_configs = [] # (is_probabilistic, class_weight, C, transformer)
+    with Pool(n_jobs) as pool:
+        for x in pool.imap(configured_fit_classifier, classifier_configs):
+            transformer_configs.extend(x)
+    transformer_configs = [ # (i_transformer, is_probabilistic, class_weight, C, transformer)
+        (x[0], x[1][0], x[1][1], x[1][2], x[1][3])
+        for x in enumerate(transformer_configs)
+    ]
+    print(f"Created {len(transformer_configs)} transformers from {len(classifier_configs)} classifiers")
+
+    # parallelize over all transformers
     configured_trial = partial(
         trial,
         trn_data = trn_data,
         val_gen = val_gen,
-        tst_gen = tst_gen,
         seed = seed,
-        n_trials = len(methods) * len(error_metrics),
+        n_trials = len(transformer_configs),
     )
-    trials = [ # (i_method, method_name, package, method, param_grid, error_metric)
-        (x[0], x[1][0][0], x[1][0][1], x[1][0][2], x[1][0][3], x[1][1])
-        for x in enumerate(itertools.product(methods, error_metrics))
-    ]
-    print(f"Starting {len(trials)} trials")
+    print(f"Starting trials for {len(transformer_configs)} transformers")
     results = []
-    with Pool() as pool:
-        results.extend(pool.imap(configured_trial, trials))
+    with Pool(n_jobs) as pool:
+        for x in pool.imap(configured_trial, transformer_configs):
+            results.extend(x)
     df = pd.DataFrame(results)
     df.to_csv(output_path) # store the results
     print(f"{df.shape[0]} results succesfully stored at {output_path}")
@@ -232,10 +182,13 @@ if __name__ == '__main__':
     parser.add_argument('output_path', type=str, help='path of an output *.csv file')
     parser.add_argument('--seed', type=int, default=876, metavar='N',
                         help='random number generator seed (default: 876)')
+    parser.add_argument('--n_jobs', type=int, default=-1, metavar='N',
+                        help='number of processes, or the number of cores (default: -1)')
     parser.add_argument("--is_test_run", action="store_true")
     args = parser.parse_args()
     main(
         args.output_path,
         args.seed,
+        args.n_jobs,
         args.is_test_run,
     )
