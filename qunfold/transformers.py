ef449f5e95137d56de75ac90df1aaf047af4940e
unknown <sadiamhjbn@gmail.com>
2023-08-30 12:11:51 +0200

    Changed the codes of transformer in transformers.py

--- qunfold/transformers.py
+++ qunfold/transformers.py
@@ -133,22 +133,19 @@ class HistogramTransformer(AbstractTransformer):
       e = np.histogram_bin_edges(x, bins=self.n_bins)[1:]
       self.edges.append(e)
     return self._transform_after_preprocessor(X), y
-  def transform(self, X):
+  def transform(self, X, average=False):
     if self.preprocessor is not None:
       X = self.preprocessor.transform(X)
-    return self._transform_after_preprocessor(X)
-  def _transform_after_preprocessor(self, X):
-    fX = []
-    for j in range(X.shape[1]): # feature index
-      e = self.edges[j]
-      i_row = np.arange(X.shape[0])
-      i_col = np.clip(np.ceil((X[:,j] - e[0]) / (e[1]-e[0])).astype(int), 0, self.n_bins-1)
-      fX_j = csr_matrix(
-        (np.ones(X.shape[0], dtype=int), (i_row, i_col)),
-        shape = (X.shape[0], self.n_bins),
-      )
-      fX.append(fX_j.toarray())
-    fX = np.stack(fX).swapaxes(0, 1).reshape((X.shape[0], -1))
-    if self.unit_scale:
-      fX = fX / fX.sum(axis=1, keepdims=True)
-    return fX
+    return self._transform_after_preprocessor(X, average=average)
+  def _transform_after_preprocessor(self, X, average=False):
+    if not average:
+        return super()._transform_after_preprocessor(X) # FIXME
+    # TODO: return concatenation of numpy histograms
+    histograms = []
+    for j in range(X.shape[1]):  # feature index
+        hist, _ = np.histogram(X[:, j], bins=self.n_bins)
+        if self.unit_scale: 
+            histograms.append(hist/(X.shape[1]*X.shape[0]))
+        else:
+            histograms.append(hist/(X.shape[0]))
+    return np.concatenate(histograms)
