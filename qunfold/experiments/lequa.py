9c6c08590af0c385af3b1bb9d9a3db21455c43c8
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2023-09-12 11:19:22 +0200

    Test the effect of gtol on prediction quality and speed

--- qunfold/experiments/lequa.py
+++ qunfold/experiments/lequa.py
@@ -1,5 +1,4 @@
 import argparse
-import itertools
 import numpy as np
 import os
 import pandas as pd
@@ -15,96 +14,47 @@ from sklearn.linear_model import LogisticRegression
 from time import time
 from tqdm.auto import tqdm
 
-# for debugging; this variant raises exceptions instead of hiding them
-import signal
-from copy import deepcopy
-class MyGridSearchQ(qp.model_selection.GridSearchQ):
-    def _delayed_eval(self, args):
-        params, training = args
-        protocol = self.protocol
-        error = self.error
-        if self.timeout > 0:
-            def handler(signum, frame):
-                raise TimeoutError()
-            signal.signal(signal.SIGALRM, handler)
-        tinit = time()
-        if self.timeout > 0:
-            signal.alarm(self.timeout)
-        try:
-            model = deepcopy(self.model)
-            model.set_params(**params)
-            model.fit(training)
-            score = qp.evaluation.evaluate(model, protocol=protocol, error_metric=error)
-            ttime = time()-tinit
-            self._sout(f'hyperparams={params}\t got {error.__name__} score {score:.5f} [took {ttime:.4f}s]')
-            if self.timeout > 0:
-                signal.alarm(0)
-        except TimeoutError:
-            self._sout(f'timeout ({self.timeout}s) reached for config {params}')
-            score = None
-        return params, score, model
-
-def trial(trial_config, trn_data, val_gen, tst_gen, seed, n_trials):
+def trial(trial_config, trn_data, tst_gen, seed, n_trials):
     """A single trial of lequa.main()"""
-    i_method, method_name, package, method, param_grid, error_metric = trial_config
+    i_method, method_name, package, method, gtol = trial_config
     np.random.seed(seed)
     print(
         f"INI [{i_method+1:02d}/{n_trials:02d}]:",
         datetime.now().strftime('%H:%M:%S'),
-        f"{package} {method_name} / {error_metric} starting"
+        f"{package} {method_name} starting"
     )
 
-    # configure and train the method; select the best hyper-parameters
-    if param_grid is not None:
-        # quapy_method = qp.model_selection.GridSearchQ(
-        quapy_method = MyGridSearchQ(
-            model = method,
-            param_grid = param_grid,
-            protocol = val_gen,
-            error = "m" + error_metric, # ae -> mae, rae -> mrae
-            refit = False,
-            # verbose = True,
-        ).fit(trn_data)
-        parameters = quapy_method.best_params_
-        val_error = quapy_method.best_score_
-        quapy_method = quapy_method.best_model_
-        print(
-            f"VAL [{i_method+1:02d}/{n_trials:02d}]:",
-            datetime.now().strftime('%H:%M:%S'),
-            f"{package} {method_name} validated {error_metric}={val_error:.4f} {parameters}"
-        )
-    else:
-        quapy_method = method.fit(trn_data)
-        parameters = None
-        val_error = -1
-        print(
-            f"Skipping validation of {package} {method_name} due to fixed hyper-parameters."
-        )
+    # train the method with fixed hyper-parameters
+    quapy_method = method.fit(trn_data)
+    print(
+        f"Skipping validation of {package} {method_name} due to fixed hyper-parameters."
+    )
 
     # evaluate the method on the test samples and return the result
     t_0 = time()
-    errors = qp.evaluation.evaluate( # errors of all predictions
-        quapy_method,
-        protocol = tst_gen,
-        error_metric = error_metric
-    )
-    prediction_time = (time() - t_0) / len(errors) # average prediction_time
-    error = errors.mean()
-    error_std = errors.std()
+    p_true, p_pred = qp.evaluation.prediction(quapy_method, protocol=tst_gen)
+    prediction_time = (time() - t_0) / len(p_true) # average prediction_time
+    p_ae = qp.error.ae(p_true, p_pred)
+    ae = p_ae.mean()
+    ae_std = p_ae.std()
+    p_rae = qp.error.rae(p_true, p_pred)
+    rae = p_rae.mean()
+    rae_std = p_rae.std()
     print(
         f"TST [{i_method+1:02d}/{n_trials:02d}]:",
         datetime.now().strftime('%H:%M:%S'),
-        f"{package} {method_name} tested {error_metric}={error:.4f}+-{error_std:.4f}"
+        f"{package} {method_name} tested",
+        f"ae={ae:.4f}+-{ae_std:.4f}, rae={rae:.4f}+-{rae_std:.4f}",
     )
     return {
         "method": method_name,
         "package": package,
-        "error_metric": error_metric,
-        "error": error,
-        "error_std": error_std,
+        "ae": ae,
+        "ae_std": ae_std,
+        "rae": rae,
+        "rae_std": rae_std,
         "prediction_time": prediction_time,
-        "val_error": val_error,
-        "parameters": str(parameters),
+        "gtol": gtol,
     }
 
 def main(
@@ -123,95 +73,63 @@ def main(
     qp.environ["N_JOBS"] = 1
 
     # configure the quantification methods
-    clf = CVClassifier(
-        LogisticRegression(),
+    gtols = [ 1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1 ]
+    clf = lambda C: CVClassifier(
+        LogisticRegression(C=C),
         n_estimators = 5,
         random_state = seed,
     )
-    clf_grid = {
-        "transformer__classifier__estimator__C": [1e-3, 1e-2, 1e-1, 1e0, 1e1],
-    }
-    qp_clf = clf.estimator
-    qp_clf_grid = {
-        "classifier__C": clf_grid["transformer__classifier__estimator__C"],
-    }
-    methods = [ # (method_name, package, method, param_grid)
-        ("ACC", "qunfold", QuaPyWrapper(ACC(clf, seed=seed)), clf_grid),
-        ("ACC", "QuaPy", qp.method.aggregative.ACC(qp_clf, val_split=5), qp_clf_grid),
-        ("PACC", "qunfold", QuaPyWrapper(PACC(clf, seed=seed)), clf_grid),
-        ("PACC", "QuaPy", qp.method.aggregative.PACC(qp_clf, val_split=5), qp_clf_grid),
-        ("HDy", "qunfold",
-            QuaPyWrapper(HDy(clf, 2, seed=seed)),
-            {
-                "transformer__preprocessor__classifier__estimator__C":
-                    clf_grid["transformer__classifier__estimator__C"],
-                "transformer__n_bins": [2, 4, 6],
-            }
-        ),
-        ("HDy", "QuaPy",
-            qp.method.aggregative.DistributionMatching(
-                classifier = qp_clf,
-                divergence = 'HD',
-                cdf = False
-            ),
-            dict(qp_clf_grid, nbins = [2, 4, 6]) # extend the qp_clf_grid
-        ),
-        ("EDy", "qunfold",
-            QuaPyWrapper(EDy(clf, seed=seed)),
-            {
-                "transformer__preprocessor__classifier__estimator__C":
-                    clf_grid["transformer__classifier__estimator__C"],
-            }
-        ),
-        ("RUN", "qunfold", QuaPyWrapper(RUN(ClassTransformer(clf), seed=seed)), clf_grid),
+    if is_test_run:
+        gtols = [ 1e-8, 1e-4, 1e-1 ]
+        clf = lambda C: CVClassifier(
+            LogisticRegression(C=C, max_iter=3),
+            n_estimators = 3,
+            random_state = seed,
+        )
+    methods = [ # (method_name, package, method, gtol)
+        ("ACC", "QuaPy", qp.method.aggregative.ACC(clf(1.0).estimator, val_split=5), -1),
+        ("PACC", "QuaPy", qp.method.aggregative.PACC(clf(10.).estimator, val_split=5), -1),
+        # ("HDy", "QuaPy",
+        #     qp.method.aggregative.DistributionMatching(
+        #         classifier = clf(.1).estimator,
+        #         divergence = 'HD',
+        #         cdf = False,
+        #         nbins = 4,
+        #     ), -1),
     ]
+    for gtol in gtols:
+        method_args = {
+            "seed": seed,
+            "solver_options": {"gtol": gtol, "maxiter": 1000},
+        }
+        methods.extend([
+            ("ACC_ae", "qunfold", QuaPyWrapper(ACC(clf(.1), **method_args)), gtol),
+            ("ACC_rae", "qunfold", QuaPyWrapper(ACC(clf(10.), **method_args)), gtol),
+            ("PACC", "qunfold", QuaPyWrapper(PACC(clf(.1), **method_args)), gtol),
+            # ("HDy", "qunfold", QuaPyWrapper(HDy(clf(.1), 6, **method_args)), gtol),
+        ])
 
     # load the data
-    trn_data, val_gen, tst_gen = qp.datasets.fetch_lequa2022(task="T1B")
+    trn_data, _, tst_gen = qp.datasets.fetch_lequa2022(task="T1B")
+
+    # for now, only a subset of the samples is used; TODO use all samples
+    tst_gen.true_prevs.df = tst_gen.true_prevs.df[:500]
 
     if is_test_run: # use a minimal testing configuration
-        clf.set_params(n_estimators = 3, estimator__max_iter = 3)
-        clf_grid = {
-            "transformer__classifier__estimator__C": [1e1],
-        }
-        qp_clf = clf.estimator
-        qp_clf_grid = {
-            "classifier__C": clf_grid["transformer__classifier__estimator__C"],
-        }
-        methods = [ # (method_name, package, method, param_grid)
-            ("ACC", "qunfold", QuaPyWrapper(ACC(clf, seed=seed)), clf_grid),
-            ("ACC", "QuaPy", qp.method.aggregative.ACC(qp_clf, val_split=3), qp_clf_grid),
-            ("PACC", "qunfold", QuaPyWrapper(PACC(clf, seed=seed)), clf_grid),
-            ("PACC", "QuaPy", qp.method.aggregative.PACC(qp_clf, val_split=3), qp_clf_grid),
-            ("HDy", "qunfold", QuaPyWrapper(HDy(clf, 2, seed=seed)), None),
-            ("HDy", "QuaPy",
-                qp.method.aggregative.DistributionMatching(
-                    classifier = qp_clf,
-                    divergence = 'HD',
-                    cdf = False
-                ),
-                None
-            ),
-            ("EDy", "qunfold", QuaPyWrapper(EDy(clf, seed=seed)), None),
-            ("RUN", "qunfold", QuaPyWrapper(RUN(ClassTransformer(clf), seed=seed)), None),
-        ]
         trn_data = trn_data.split_stratified(3000, random_state=seed)[0] # subsample
-        val_gen.true_prevs.df = val_gen.true_prevs.df[:3] # use only 3 validation samples
         tst_gen.true_prevs.df = tst_gen.true_prevs.df[:3] # use only 3 testing samples
 
     # parallelize over all methods
-    error_metrics = ['ae', 'rae']
     configured_trial = partial(
         trial,
         trn_data = trn_data,
-        val_gen = val_gen,
         tst_gen = tst_gen,
         seed = seed,
-        n_trials = len(methods) * len(error_metrics),
+        n_trials = len(methods),
     )
-    trials = [ # (i_method, method_name, package, method, param_grid, error_metric)
-        (x[0], x[1][0][0], x[1][0][1], x[1][0][2], x[1][0][3], x[1][1])
-        for x in enumerate(itertools.product(methods, error_metrics))
+    trials = [ # (i_method, method_name, package, method, gtol)
+        (x[0], x[1][0], x[1][1], x[1][2], x[1][3])
+        for x in enumerate(methods)
     ]
     print(f"Starting {len(trials)} trials")
     results = []
