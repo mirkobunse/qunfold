734aeeb35534023b8b6f810c135cfeb6d9164728
Sadia Mahjabin <sadiamhjbn@gmail.com>
2023-10-19 15:51:20 +0200

    Histogram concatenation for a faster HistogramTransformer (#2)
    * Changed the codes of transformer in transformers.py
    
    * Corrected transform_after_preprocessor
    
    * Added a unit test for average=True
    
    * Test the transformer in performance_HDx; it does not produce correct results, yet
    
    * Corrected the histogram bins when average is false
    
    * Improving whitespace and comments
    
    ---------
    
    Co-authored-by: Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>

--- qunfold/transformers.py
+++ qunfold/transformers.py
@@ -130,25 +130,37 @@ class HistogramTransformer(AbstractTransformer):
       self.n_classes = len(np.unique(y))
     self.edges = []
     for x in X.T: # iterate over columns = features
-      e = np.histogram_bin_edges(x, bins=self.n_bins)[1:]
+      e = np.histogram_bin_edges(x, bins=self.n_bins)
       self.edges.append(e)
     return self._transform_after_preprocessor(X), y
-  def transform(self, X):
+  def transform(self, X, average=False):
     if self.preprocessor is not None:
       X = self.preprocessor.transform(X)
-    return self._transform_after_preprocessor(X)
-  def _transform_after_preprocessor(self, X):
-    fX = []
-    for j in range(X.shape[1]): # feature index
-      e = self.edges[j]
-      i_row = np.arange(X.shape[0])
-      i_col = np.clip(np.ceil((X[:,j] - e[0]) / (e[1]-e[0])).astype(int), 0, self.n_bins-1)
-      fX_j = csr_matrix(
-        (np.ones(X.shape[0], dtype=int), (i_row, i_col)),
-        shape = (X.shape[0], self.n_bins),
-      )
-      fX.append(fX_j.toarray())
-    fX = np.stack(fX).swapaxes(0, 1).reshape((X.shape[0], -1))
-    if self.unit_scale:
-      fX = fX / fX.sum(axis=1, keepdims=True)
-    return fX
+    return self._transform_after_preprocessor(X, average=average)
+  def _transform_after_preprocessor(self, X, average=False):
+    if not average:
+      fX = []
+      for j in range(X.shape[1]): # feature index
+        e = self.edges[j][1:]
+        i_row = np.arange(X.shape[0])
+        i_col = np.clip(np.ceil((X[:,j] - e[0]) / (e[1]-e[0])).astype(int), 0, self.n_bins-1)
+        fX_j = csr_matrix(
+          (np.ones(X.shape[0], dtype=int), (i_row, i_col)),
+          shape = (X.shape[0], self.n_bins),
+        )
+        fX.append(fX_j.toarray())
+      fX = np.stack(fX).swapaxes(0, 1).reshape((X.shape[0], -1))
+      if self.unit_scale:
+        fX = fX / fX.sum(axis=1, keepdims=True)
+      return fX
+    else: # a concatenation of numpy histograms is faster to compute
+      histograms = []
+      for j in range(X.shape[1]):  # feature index
+        e = np.copy(self.edges[j])
+        e[0] = -np.inf # always use exactly self.n_bins and never omit any items
+        e[-1] = np.inf
+        hist, _ = np.histogram(X[:, j], bins=e)
+        if self.unit_scale:
+          hist = hist / X.shape[1]
+        histograms.append(hist / X.shape[0])
+      return np.concatenate(histograms)
