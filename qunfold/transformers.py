Corrected the histogram bins when average is false

qunfold/transformers.py
@@ -130,7 +130,7 @@ class HistogramTransformer(AbstractTransformer):
       self.n_classes = len(np.unique(y))
     self.edges = []
     for x in X.T: # iterate over columns = features
-      e = np.histogram_bin_edges(x, bins=self.n_bins)[1:]
+      e = np.histogram_bin_edges(x, bins=self.n_bins)
       self.edges.append(e)
     return self._transform_after_preprocessor(X), y
   def transform(self, X, average=False):
@@ -141,7 +141,7 @@ class HistogramTransformer(AbstractTransformer):
     if not average:
         fX = []
         for j in range(X.shape[1]): # feature index
-          e = self.edges[j]
+          e = self.edges[j][1:]
           i_row = np.arange(X.shape[0])
           i_col = np.clip(np.ceil((X[:,j] - e[0]) / (e[1]-e[0])).astype(int), 0, self.n_bins-1)
           fX_j = csr_matrix(
@@ -155,8 +155,13 @@ class HistogramTransformer(AbstractTransformer):
         return fX
     # return concatenation of numpy histograms
     histograms = []
+  
     for j in range(X.shape[1]):  # feature index
-        hist, _ = np.histogram(X[:, j], bins=self.n_bins)
+        e= np.copy(self.edges[j])
+        e[0]=-np.inf
+        e[-1]=np.inf
+        
+        hist, _ = np.histogram(X[:, j], bins=e)
         if self.unit_scale: 
             histograms.append(hist/(X.shape[1]*X.shape[0]))
         else: