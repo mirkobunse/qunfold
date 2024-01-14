e4f74144e813d6e76cec5d1ec3e5eb7f98be2bed
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2024-01-14 19:14:55 +0100

    Flax experiment with multiple configurations

---
+++ qunfold/experiments/flax/__init__.py
@@ -0,0 +1,170 @@
+import flax
+import jax
+import jax.numpy as jnp
+import numpy as np
+import optax
+import quapy as qp
+import qunfold
+from clu import metrics
+from flax import linen as nn
+from flax.training import train_state
+from typing import Callable, Sequence
+
+
+
+######## code of git@github.com:mirkobunse/acspy.git ########
+
+def n_samples_per_class(y, n_classes=None):
+  """Determine the number of instances per class.
+
+  Args:
+      y: An array of labels, shape (n_samples,).
+      n_classes (optional): The number of classes. Defaults to `None`, which corresponds to `len(np.unique(y))`.
+
+  Returns:
+      An array of label counts, shape (n_classes,).
+  """
+  if n_classes is None:
+    n_classes = len(np.unique(y))
+  n_samples_per_class = np.zeros(n_classes, dtype=int)
+  i, n = np.unique(y, return_counts=True)
+  n_samples_per_class[i] = n # non-existing classes maintain a zero entry
+  return n_samples_per_class
+
+def draw_indices(
+    y,
+    score,
+    m = None,
+    n_classes = None,
+    allow_duplicates = False,
+    allow_others = True,
+    allow_incomplete = True,
+    min_samples_per_class = 0,
+    random_state = None,
+  ):
+  """Draw m indices of instances, according to the given class scores.
+
+  Args:
+      y: An pool of labels, shape (n_samples,).
+      score: An array of utility scores for the acquisition of each clas, shape (n_classes,).
+      m (optional): The total number of instances to be drawn. Defaults to `None`, which is only valid for binary classification tasks and which corresponds to drawing the maximum number of instances for which the scores can be fulfilled.
+      n_classes (optional): The number of classes. Defaults to `None`, which corresponds to `len(np.unique(y))`.
+      allow_duplicates (optional): Whether to allow drawing one sample multiple times. Defaults to `False`.
+      allow_others (optional): Whether to allow drawing other classes if the desired class is exhausted. Defaults to `True`.
+      allow_incomplete (optional): Whether to allow an incomplete draw if all classes are exhausted. Defaults to `True`.
+      min_samples_per_class (optional): The minimum number of samples per class. Defaults to `0`.
+      random_state (optional): A numpy random number generator, or a seed thereof. Defaults to `None`, which corresponds to `np.random.default_rng()`.
+
+  Returns:
+      An array of indices, shape (m,).
+  """
+  if n_classes is None:
+    n_classes = len(np.unique(y))
+  elif len(score) != n_classes:
+    raise ValueError("len(score) != n_classes")
+  random_state = np.random.default_rng(random_state)
+  m_pool = n_samples_per_class(y, n_classes) # number of available samples per class
+  p = np.maximum(0, score) / np.maximum(0, score).sum() # normalize scores to probabilities
+  if not np.isfinite(p).all():
+    raise ValueError(f"NaN probabilities caused by score={score}")
+
+  # determine the number of instances to acquire from each class
+  if m is not None:
+    to_take = np.minimum(m_pool, np.maximum(np.round(m*p).astype(int), min_samples_per_class))
+    while m != to_take.sum(): # rarely takes more than one iteration
+      m_remaining = m - to_take.sum()
+      if m_remaining > 0: # are additional draws needed?
+        i = np.nonzero(to_take < m_pool)[0]
+        if len(i) == 0:
+          if allow_incomplete:
+            break
+          else:
+            raise ValueError(
+              f"All classes are exhausted; consider setting allow_incomplete=True"
+            )
+        elif allow_others or len(i) == len(p):
+          bincount = np.bincount(random_state.choice(
+            len(i),
+            size = m_remaining,
+            p = np.maximum(1/m, p[i]) / np.maximum(1/m, p[i]).sum(),
+          ))
+          to_take[i[:len(bincount)]] += bincount
+        else:
+          raise ValueError(
+            f"Class {np.setdiff1d(np.arange(len(p)), i)[0]} exhausted; "
+            "consider setting allow_others=True "
+          )
+      elif m_remaining < 0: # are less draws needed?
+        i = np.nonzero(to_take > min_samples_per_class)[0]
+        bincount = np.bincount(random_state.choice(
+          len(i),
+          size = -m_remaining, # negate to get a positive value
+          p = np.maximum(1/m, 1-p[i]) / np.maximum(1/m, 1-p[i]).sum(),
+        ))
+        to_take[i[:len(bincount)]] -= bincount
+      to_take = np.maximum(min_samples_per_class, np.minimum(to_take, m_pool))
+  else: # m is None
+    if len(m_pool) != 2:
+      raise ValueError("m=None is only valid for binary classification tasks")
+    to_take = m_pool.copy()
+    if p[0] > m_pool[0] / m_pool.sum(): # do we have to increase the class "0" probability?
+      to_take[1] = int(np.round(m_pool[0] * p[1] / p[0])) # sub-sample class "1"
+    else: # otherwise, we need to increase the class "1" probability
+      to_take[0] = int(np.round(m_pool[1] * p[0] / p[1])) # sub-sample class "0"
+    to_take = np.minimum(to_take, m_pool)
+
+  # draw indices that represent instances of the classes to acquire
+  i_rand = random_state.permutation(len(y)) # random order after shuffling
+  i_draw = [] # array of drawn index arrays (relative to i_rand)
+  for c in range(n_classes):
+    i_c = np.arange(len(y))[y[i_rand] == c]
+    if to_take[c] <= len(i_c):
+      i_draw.append(i_c[:to_take[c]])
+    elif allow_duplicates:
+      i_c = np.tile(i_c, int(np.ceil(to_take[c] / len(i_c)))) # repeat i_c multiple times
+      i_draw.append(i_c[:to_take[c]]) # draw from the tiled array
+    else:
+      raise ValueError(
+        f"Class {c} exhausted; consider setting allow_duplicates=True "
+        f"({to_take[c]} requested, {len(i_c)} available)"
+      )
+  return i_rand[np.concatenate(i_draw)]
+
+######## end of git@github.com:mirkobunse/acspy.git  ########
+
+
+
+class SimpleModule(nn.Module):
+  """A simple dense neural network with ReLU activation."""
+  n_features: Sequence[int]
+  @nn.compact
+  def __call__(self, x):
+    for i, n_features in enumerate(self.n_features):
+      x = nn.Dense(n_features)(nn.activation.relu(x) if i > 0 else x)
+    return x
+
+# a storage for all state involved in training, including metrics
+@flax.struct.dataclass
+class Metrics(metrics.Collection):
+  loss: metrics.Average.from_output("loss") # average loss
+class TrainingState(train_state.TrainState):
+  metrics: Metrics
+
+def create_training_state(module, rng, learning_rate, momentum):
+  """Create an initial `TrainingState`."""
+  return TrainingState.create(
+    apply_fn = module.apply,
+    params = module.init( # initialize parameters
+      rng,
+      jnp.ones((1, 300)) # a template batch with one sample, 300 dimensions
+    )["params"],
+    tx = optax.sgd(learning_rate, momentum),
+    metrics = Metrics.empty()
+  )
+
+def _draw_indices_parallel(args, y, m): # args = (seed, p_T)
+  return draw_indices(y, args[1], m=m, random_state=args[0])
+
+def _solve_parallel(args, M): # args = (q, p_true)
+  p_est = qunfold.GenericMethod(qunfold.LeastSquaresLoss(), None).solve(args[0], M)
+  return qp.error.ae(args[1], p_est)
---
+++ qunfold/experiments/flax/main.py
@@ -0,0 +1,208 @@
+import argparse
+import itertools
+import jax
+import jax.numpy as jnp
+import numpy as np
+import pandas as pd
+import quapy as qp
+from flax import linen as nn
+from functools import partial
+from multiprocessing import Pool
+from sklearn.model_selection import train_test_split
+from time import time
+from . import (
+  n_samples_per_class,
+  draw_indices,
+  SimpleModule,
+  create_training_state,
+  _draw_indices_parallel,
+  _solve_parallel,
+)
+
+# TODO implement a learning rate schedule with validation plateau-ing:
+# https://github.com/HDembinski/essays/blob/master/regression.ipynb
+
+def _main( # one trial of the experiment; to be taken out with multiple configurations
+    n_features = [64],
+    learning_rate = 1,
+    n_batches = 500,
+    batch_size = 64,
+    sample_size = 1000,
+    n_batches_between_evaluations = 10, # how many samples to process between evaluations
+    n_jobs = 8,
+    n_val = 8,
+  ):
+  trn_data, val_gen, tst_gen = qp.datasets.fetch_lequa2022(task="T1B")
+  X_trn, y_trn = trn_data.Xy
+  X_trn = jnp.array(X_trn, dtype=jnp.float32)
+  y_trn = jnp.array(y_trn)
+  p_trn = n_samples_per_class(y_trn) / len(y_trn)
+  avg_trn = np.zeros((28, len(y_trn))) # shape (n_classes, n_samples)
+  for i in range(28):
+    avg_trn[i, y_trn == i] = 1 / np.sum(y_trn == i)
+  avg_trn = jnp.array(avg_trn, dtype=jnp.float32)
+  val_gen.true_prevs.df = val_gen.true_prevs.df[:n_val] # use only some validation samples
+  X_val = []
+  p_val = []
+  for X_i, p_i in val_gen():
+    X_val.append(X_i)
+    p_val.append(p_i)
+  X_val = jnp.vstack(X_val)
+  p_val = jnp.vstack(p_val)
+  avg_val = np.zeros((p_val.shape[0], X_val.shape[0]))
+  for i in range(p_val.shape[0]):
+    avg_val[i,i*1000:(i+1)*1000] = 1 / 1000 # lequa uses 1000 items per sample
+  avg_val = jnp.array(avg_val, dtype=jnp.float32)
+
+  # instantiate the model
+  module = SimpleModule(n_features)
+  print(module.tabulate( # inspect the structure of the model
+    jax.random.key(0),
+    jnp.ones((1, 300)),
+    compute_flops = True,
+    compute_vjp_flops = True
+  ))
+  training_state = create_training_state(
+    module,
+    jax.random.key(0),
+    learning_rate = learning_rate,
+    momentum = .9,
+  )
+
+  # take out the training
+  print("Training...")
+  X_M, X_q, y_M, y_q = train_test_split(
+    X_trn,
+    y_trn,
+    stratify = y_trn,
+    test_size = .5,
+    random_state = 25
+  )
+  avg_M = np.zeros((28, len(y_M))) # shape (n_classes, n_samples)
+  for i in range(28):
+    avg_M[i, y_M == i] = 1 / np.sum(y_M == i)
+  avg_M = jnp.array(avg_M, dtype=jnp.float32)
+  avg_q = np.zeros((batch_size, batch_size * sample_size))
+  for i in range(batch_size):
+    avg_q[i,i*sample_size:(i+1)*sample_size] = 1 / sample_size
+  avg_q = jnp.array(avg_q, dtype=jnp.float32)
+  sample_rng = np.random.default_rng(25)
+  metrics_history = {
+    "trn_loss": [],
+  }
+
+  @jax.jit
+  def training_step(state, p_Ts, X_q_i):
+    """Perform a single parameter update.
+
+    Returns:
+        An updated TrainingState object.
+    """
+    def loss_fn(params):
+      qs = jnp.dot(avg_q, nn.activation.sigmoid(state.apply_fn({ "params": params }, X_q_i)))
+      M = jnp.dot(avg_M, nn.activation.sigmoid(state.apply_fn({ "params": params }, X_M))).T
+      v = p_Ts - (jnp.linalg.pinv(M) @ qs.T).T # p_T - p_hat for each p_T
+      return (v**2).sum(axis=1).mean() # least squares ||p* - pinv(M)q||
+    loss, grad = jax.value_and_grad(loss_fn)(state.params)
+    state = state.apply_gradients(grads=grad) # update the state
+    metric_updates = state.metrics.single_from_model_output(loss=loss)
+    metrics = state.metrics.merge(metric_updates)
+    return state.replace(metrics=metrics) # update the state with metrics
+
+  @jax.jit
+  def validation_embedding(state):
+    qs = jnp.dot(
+      avg_val,
+      nn.activation.sigmoid(state.apply_fn({ "params": state.params }, X_val)
+    ))
+    M = jnp.dot(
+      avg_trn,
+      nn.activation.sigmoid(state.apply_fn({ "params": state.params }, X_trn)
+    )).T
+    return qs, M
+
+  results = []
+  t_0 = time()
+  for batch_index in range(n_batches):
+    if batch_index == 1:
+      t_0 = time() # reset after the first batch to ignore JIT overhead
+
+    # update parameters and metrics
+    p_Ts = sample_rng.dirichlet(np.ones(28), size=batch_size)
+    q_i = []
+    draw_indices_parallel = partial(_draw_indices_parallel, y=y_q, m=sample_size)
+    with Pool(n_jobs if n_jobs > 0 else None) as pool:
+      q_i.extend(pool.imap(draw_indices_parallel, enumerate(p_Ts)))
+    p_Ts = jnp.vstack([ n_samples_per_class(y_q[i], 28) / sample_size for i in q_i ])
+    X_q_i = jnp.vstack([ X_q[i] for i in q_i ])
+    training_state = training_step(training_state, p_Ts, X_q_i)
+
+    # compute average training metrics for this epoch and reset the metric state
+    for metric, value in training_state.metrics.compute().items():
+      metrics_history[f"trn_{metric}"].append(value)
+    training_state = training_state.replace(metrics=training_state.metrics.empty()) # reset
+
+    # evaluate every n_batches_between_evaluations
+    if batch_index % n_batches_between_evaluations == 0 or (batch_index+1) == n_batches:
+      qs, M = validation_embedding(training_state)
+      solve_parallel = partial(_solve_parallel, M=M)
+      errors = []
+      with Pool(n_jobs if n_jobs > 0 else None) as pool:
+        errors.extend(pool.imap(solve_parallel, zip(qs, p_val)))
+      error = np.mean(errors)
+      error_std = np.std(errors)
+      trn_loss = np.array(metrics_history["trn_loss"])[-n_batches_between_evaluations:].mean()
+      it_per_s = max(batch_index, 1) / (time() - t_0)
+      results.append({
+        "n_features": str(n_features),
+        "learning_rate": learning_rate,
+        "batch_index": batch_index+1,
+        "mae": error,
+        "mae_std": error_std,
+        "trn_loss": trn_loss,
+        "it_per_s": it_per_s,
+      })
+      print(
+        f"[{batch_index+1:2d}/{n_batches}] ",
+        f"MAE={error:.5f}+-{error_std:.5f}",
+        f", trn_loss={trn_loss:e}",
+        f", {it_per_s:.3f} it/s",
+        sep = "",
+      )
+  return results
+
+def main(
+    output_path,
+    is_test_run = False
+  ):
+  # configure the experiments
+  n_features = [ [64], [128], [64, 64], [128, 128], [64, 64, 64], [128, 128, 128] ]
+  learning_rates = [ 1e-2, 1e-1, 1e0, 1e1 ]
+  kwargs = {}
+  if is_test_run:
+    n_features = [ [64], [128] ]
+    learning_rates = [ 1e0 ]
+    kwargs["n_batches"] = 3
+    kwargs["n_batches_between_evaluations"] = 3
+    kwargs["batch_size"] = 8
+    kwargs["n_val"] = 3
+
+  # run all experiments one after another
+  results = []
+  for args in itertools.product(n_features, learning_rates):
+    results.extend(_main(*args, **kwargs))
+  results = pd.DataFrame(results)
+
+  # store the results
+  results.to_csv(output_path)
+  print(f"{results.shape[0]} results succesfully stored at {output_path}")
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument('output_path', type=str, help='path of an output *.csv file')
+  parser.add_argument("--is_test_run", action="store_true")
+  args = parser.parse_args()
+  main(
+      args.output_path,
+      args.is_test_run,
+  )
