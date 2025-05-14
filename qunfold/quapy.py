d47963755b8cac7da9b53126c78d12ca98ee2d2d
Alejandro Moreo <alejandro.moreo@isti.cnr.it>
2025-05-14 15:14:39 +0200

    adapting qunfold for the new interface of quapy in which labelled collection has disappeared from the fit method in favor of X, y

--- qunfold/quapy.py
+++ qunfold/quapy.py
@@ -1,39 +1,45 @@
 import inspect
-from collections import defaultdict
 from dataclasses import dataclass
 from quapy.method.base import BaseQuantifier
-from . import AbstractMethod, LinearMethod
+from . import AbstractMethod
 from .base import BaseMixin
 
+
 @dataclass
-class QuaPyWrapper(BaseQuantifier,BaseMixin):
-  """A thin wrapper for using qunfold methods in QuaPy.
+class QuaPyWrapper(BaseQuantifier, BaseMixin):
+    """A thin wrapper for using qunfold methods in QuaPy.
 
-  Args:
-    _method: An instance of `qunfold.methods.AbstractMethod` to wrap.
+    Args:
+      _method: An instance of `qunfold.methods.AbstractMethod` to wrap.
 
-  Examples:
-    Here, we wrap an instance of ACC to perform a grid search with QuaPy.
+    Examples:
+      Here, we wrap an instance of ACC to perform a grid search with QuaPy.
 
-      >>> qunfold_method = QuaPyWrapper(ACC(RandomForestClassifier(obb_score=True)))
-      >>> quapy.model_selection.GridSearchQ(
-      >>>     model = qunfold_method,
-      >>>     param_grid = { # try both splitting criteria
-      >>>         "representation__classifier__estimator__criterion": ["gini", "entropy"],
-      >>>     },
-      >>>     # ...
-      >>> )
-  """
-  _method: AbstractMethod
-  def fit(self, data): # data is a qp.LabelledCollection
-    self._method.fit(*data.Xy, data.n_classes)
-    return self
-  def quantify(self, X):
-    return self._method.predict(X)
-  def set_params(self, **params):
-    self._method.set_params(**params)
-    return self
-  def get_params(self, deep=True):
-    return self._method.get_params(deep)
-  def __str__(self):
-    return self._method.__str__()
+        >>> qunfold_method = QuaPyWrapper(ACC(RandomForestClassifier(obb_score=True)))
+        >>> quapy.model_selection.GridSearchQ(
+        >>>     model = qunfold_method,
+        >>>     param_grid = { # try both splitting criteria
+        >>>         "representation__classifier__estimator__criterion": ["gini", "entropy"],
+        >>>     },
+        >>>     # ...
+        >>> )
+    """
+    _method: AbstractMethod
+
+    def fit(self, X, y):
+        self._method.fit(X, y, n_classes=len(set(y)))
+        return self
+
+    def predict(self, X):
+        return self._method.predict(X)
+
+    def set_params(self, **params):
+        self._method.set_params(**params)
+        return self
+
+    def get_params(self, deep=True):
+        return self._method.get_params(deep)
+
+    def __str__(self):
+        return self._method.__str__()
+
